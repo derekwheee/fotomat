69ba963114f2c5743eac604c33875a8bc93bef99
Derek Wheelden <derek.wheelden@gmail.com>
2015-06-06 11:12:47 -0400

    Trying to get Facebook share to work

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var fs = require('fs');
 var app = express();
 var server;
 var ws;
